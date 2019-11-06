768632f6402536ff6c938d7c0c41e30be147daf6
mikeromero4 <mikeromero4@Yahoo.com>
2019-11-06 14:02:36 -0800

    converted functional animation component to a class component (due to overuse of a react hooks)

--- src/components/UI/animation/index.js
+++ src/components/UI/animation/index.js
@@ -1,52 +1,52 @@
-
 import React from "react"
-import { useState, useEffect } from "react"
 import "./style.scss"
 import Waves from "./sineWave"
 import Icons from "./icons"
-import { relative } from "path"
-let invocationsPerSecond=0; //test
-window.setInterval(function(){
-  console.log(invocationsPerSecond)
-  invocationsPerSecond=0
-},1000)
-//this should probably(?) be a class component due to increased use of hooks
-export default props => {
-  let [time, setTimer] = useState(0)
-  const requestRef = React.useRef()
-  const el = React.useRef()
-  let width = el.current ? el.current.offsetWidth : 0
-  let height = el.current ? el.current.offsetHeight : 0
 
-  useEffect(function() {
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-      
-    requestRef.current = requestAnimationFrame(animate)
-    return () => cancelAnimationFrame(requestRef.current)
-  }, [])
-  function animate(time) {
-    setTimer(time / 10)
-    invocationsPerSecond++
-    console.log('d')
-    requestRef.current = requestAnimationFrame(animate)
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { time: 0 }
+    this.element = React.createRef()
+    this.animate = this.animate.bind(this)
   }
-
-  let settings = {
-    ...props.settings,
-    iconSize:50,
-    width,
-    height,
+  componentDidMount() {
+    // Set the CSS value of the --vh custom property to the root of the document
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+    this.animationFrameReference = requestAnimationFrame(this.animate)
   }
-  return (
-    <div className="animation">
-          <div ref={el} style={{position:'relative', height: "100%" }}>
-      <div style={{position:'absolute',bottom: (height*(35/100) + settings.iconSize/2 + 50 +"px") }} >
-        <Icons  settings={settings} time={time} />
+  componentWillUnmount() {
+    cancelAnimationFrame(this.animationFrameReference)
+  }
+  animate(time) {
+    this.setState({ time: time / 10 })
+    this.animationFrameReference = requestAnimationFrame(this.animate)
+  }
+  render() {
+    const { props,element:{current} } = this
+    let width = current ? current.offsetWidth : 0
+    let height = current ? current.offsetHeight : 0
+    let settings = {
+      ...props.settings,
+      iconSize: 50,
+      width,
+      height,
+    }
+    return (
+      <div className="animation">
+        <div ref={this.element} style={{ position: "relative", height: "100%" }}>
+          <div
+            style={{
+              position: "absolute",
+              bottom: height * (35 / 100) + settings.iconSize / 2 + 50 + "px",
+            }}
+          >
+            <Icons settings={settings} time={this.state.time} />
+          </div>
+        </div>
+        <Waves settings={settings} time={this.state.time} />
       </div>
-      </div>
-      <Waves settings={settings} time={time} />
-    </div>
-  )
+    )
+  }
 }
